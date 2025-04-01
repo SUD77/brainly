5bbec466973b71ade09fa5fc58b1ba428c1f1443
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-04-01 10:39:26 +0530

    updated endpoint to post content

--- src/db.ts
+++ src/db.ts
@@ -22,7 +22,7 @@ const contentSchema=new Schema({
   type: {type: String, enum: contentTypes, required: true},
   title: {type: String, required: true},
   tags: [{type: Types.ObjectId, ref:'Tag'}],
-  userId: {type: Types.ObjectId, ref: 'User', required: true}
+  userId: {type: Types.ObjectId, ref: 'User'}
 });
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { UserModel } from "./db";
+import { ContentModel, UserModel } from "./db";
 import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
 const app = express();
@@ -56,9 +56,31 @@ app.post("/api/v1/signin", async (req, res) => {
   }
 });
 
-app.post("/api/v1/content", userMiddleware, (req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
   const link = req.body.link;
   const type = req.body.type;
+  const title=req.body.title;
+
+  try{
+
+    await ContentModel.create({
+      link,
+      type,
+      title,
+      //@ts-ignore
+      userId: req.userId,
+      tags: []
+    })
+  
+    res.json({
+      message: "Content added"
+    })
+
+  }catch(e){
+    console.log(e);
+  }
+
+ 
 });
 
 app.get("/api/v1/content", (req, res) => {});
